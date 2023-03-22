846d2175ebdb4d56ff0c89ece91b28b8ccdf846b
ClementChaumel <clement.chaumel@gmail.com>
2023-03-22 17:58:42 +0100

    Generate a new Stencil project

---
+++ stencil.config.ts
@@ -0,0 +1,23 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'vanilla-web-components',
+  plugins: [sass()],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
